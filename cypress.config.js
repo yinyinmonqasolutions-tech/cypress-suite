d497e4121555642a8f2d1217bc820d48ccea9cad
yinyinmon <yinyinmon661@gmail.com>
2026-06-12 14:57:53 +0700

    fix: update cypress.config.js for Cypress 15

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+    allowCypressEnv: true,
+
   // ─── Reporter ───────────────────────────────────
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
